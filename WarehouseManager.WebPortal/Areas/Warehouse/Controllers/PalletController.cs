d46c3947974102559481f7b254d8c59782fbfcb5
cotobian <cotobian@gmail.com>
2023-08-04 14:06:42 +0700

    update0408

--- WarehouseManager.WebPortal/Areas/Warehouse/Controllers/PalletController.cs
+++ WarehouseManager.WebPortal/Areas/Warehouse/Controllers/PalletController.cs
@@ -26,26 +26,19 @@ namespace WarehouseManager.WebPortal.Areas.Warehouse.Controllers
             {
                 BarcodeWriterPixelData barcodeWriter = new BarcodeWriterPixelData();
                 EncodingOptions encodingOptions = new EncodingOptions();
-                encodingOptions.Height = 300;
+                encodingOptions.Height = 250;
                 encodingOptions.Width = 600;
                 barcodeWriter.Options = encodingOptions;
                 barcodeWriter.Format = BarcodeFormat.CODE_128;
-
-                // Tạo hình ảnh mã vạch dưới dạng mảng byte
                 PixelData pixelData = barcodeWriter.Write(content);
 
-                // Tạo Bitmap từ dữ liệu pixel
                 using (Bitmap barcodeImage = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppRgb))
                 {
                     BitmapData bitmapData = barcodeImage.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
                     Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
                     barcodeImage.UnlockBits(bitmapData);
-
-                    // Lưu Bitmap thành tệp ảnh
                     string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Content", "forms", "images", content + ".png");
                     barcodeImage.Save(imagePath, ImageFormat.Png);
-
-                    // Trả về đường dẫn tới tệp ảnh
                     return imagePath;
                 }
             }
@@ -97,60 +90,58 @@ namespace WarehouseManager.WebPortal.Areas.Warehouse.Controllers
         [HttpGet]
         public async Task<IActionResult> PrintMultiplePallet(string palletIds)
         {
-            try
+            List<string> fileNames = new List<string>();
+            List<byte[]> fileData = new List<byte[]>();
+            string formsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Content", "forms");
+            string filePath = Path.Combine(formsPath, "PALLETSHEET.xlsx");
+            string imagePath = Path.Combine(formsPath, "images");
+            string palletPath = Path.Combine(formsPath, "palletsheets");
+
+            string[] palletsheets = Directory.GetFiles(palletPath);
+            foreach (string file in palletsheets)
             {
-                List<string> fileNames = new List<string>();
-                List<byte[]> fileData = new List<byte[]>();
-                string formsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Content", "forms");
-                string filePath = Path.Combine(formsPath, "PALLETSHEET.xlsx");
-                string imagePath = Path.Combine(formsPath, "images");
-                string palletPath = Path.Combine(formsPath, "palletsheets");
-
-                string[] palletsheets = Directory.GetFiles(palletPath);
-                foreach (string file in palletsheets)
-                {
-                    System.IO.File.Delete(file);
-                }
-
-                string[] images = Directory.GetFiles(imagePath);
-                foreach (string img in images)
-                {
-                    System.IO.File.Delete(img);
-                }
-
-                foreach (string pallet in palletIds.Split(','))
-                {
-                    string excelPath = Path.Combine(formsPath, "palletsheets", pallet + ".xlsx");
-                    System.IO.File.Copy(filePath, excelPath);
-                    FileInfo fileInfo = new FileInfo(excelPath);
-                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                    using (var package = new ExcelPackage(fileInfo))
-                    {
-                        var workSheet = package.Workbook.Worksheets[0];
-                        workSheet.Cells["E1"].Value = pallet;
-                        string imgPath = GenerateBarcode(pallet);
-                        var picture = workSheet.Drawings.AddPicture(pallet, imgPath);
-                        picture.SetPosition(1, 0, 2, 0);
-                        package.Save();
-                        fileData.Add(package.GetAsByteArray());
-                    }
-                    fileNames.Add(pallet + ".xlsx");
-                }
-
-                var zipStream = new MemoryStream();
-                var zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Create);
-                for (int i = 0; i < fileNames.Count; i++)
-                {
-                    var entry = zipArchive.CreateEntry(fileNames[i]);
-                    using var entryStream = entry.Open();
-                    entryStream.Write(fileData[i], 0, fileData[i].Length);
-                }
-                zipStream.Position = 0;
-                return File(zipStream, "application/zip", "ExcelFiles.zip");
+                System.IO.File.Delete(file);
             }
-            catch (Exception ex)
+
+            string[] images = Directory.GetFiles(imagePath);
+            foreach (string img in images)
             {
-                return BadRequest();
+                System.IO.File.Delete(img);
+            }
+
+            foreach (string pallet in palletIds.Split(','))
+            {
+                string excelPath = Path.Combine(formsPath, "palletsheets", pallet + ".xlsx");
+                System.IO.File.Copy(filePath, excelPath);
+                FileInfo fileInfo = new FileInfo(excelPath);
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (var package = new ExcelPackage(fileInfo))
+                {
+                    var workSheet = package.Workbook.Worksheets[0];
+                    workSheet.Cells["E1"].Value = pallet;
+                    string imgPath = GenerateBarcode(pallet);
+                    var picture = workSheet.Drawings.AddPicture(pallet, imgPath);
+                    picture.SetPosition(6, 5, 8, 5);
+                    package.Save();
+                    fileData.Add(package.GetAsByteArray());
+                }
+                fileNames.Add(pallet + ".xlsx");
+            }
+
+            using (var zipStream = new MemoryStream())
+            {
+                using (var zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Create, true))
+                {
+                    for (int i = 0; i < fileNames.Count; i++)
+                    {
+                        var entry = zipArchive.CreateEntry(fileNames[i], System.IO.Compression.CompressionLevel.Fastest);
+                        using (var entryStream = entry.Open())
+                        {
+                            entryStream.Write(fileData[i], 0, fileData[i].Length);
+                        }
+                    }
+                }
+                return File(zipStream.ToArray(), "application/zip", "Pallets.xlsx");
             }
         }
     }
